e2c68fe10ee6b0e1c4f54fb8f8828cadc2408229
sojeong2020 <73712403+sojeong2020@users.noreply.github.com>
2021-03-29 23:58:02 +0100

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("HELLO");
-  const [isMouseOver, setMouseOver] = useState(false);
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
 
-  function handleClick() {
-    setHeadingText("Submitted");
-  }
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-  function handleMouseOut() {
-    setMouseOver(false);
-  }
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-        onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <input name="email" placeholder="Email" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
